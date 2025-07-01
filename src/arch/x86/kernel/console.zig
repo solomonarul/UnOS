6aea6cd9052d712ffefb609d7b1432d32452f23b
Solomonarul <solomonarul@tuta.io>
2025-07-02 02:35:21 +0300

    fix(console): Revert back ports.

--- src/arch/x86/kernel/console.zig
+++ src/arch/x86/kernel/console.zig
@@ -1,6 +1,6 @@
 const fmt = @import("std").fmt;
 const Writer = @import("std").io.Writer;
-const Port = @import("ports.zig");
+const Ports = @import("ports.zig");
 
 const VGA_WIDTH = 80;
 const VGA_HEIGHT = 25;
@@ -28,42 +28,27 @@ const ConsoleColors = enum(u8) {
 var x: usize = 0;
 var y: usize = 0;
 var color = make_color(ConsoleColors.LightGray, ConsoleColors.Black);
-var buffer: [*]volatile u16 = @as([*]volatile u16, @ptrFromInt(0xB8000));
+var base: [*]volatile u16 = @as([*]volatile u16, @ptrFromInt(0xB8000));
 
 fn make_color(fg: ConsoleColors, bg: ConsoleColors) u8 {
     return @as(u8, @intFromEnum(fg)) | (@as(u8, @intFromEnum(bg)) << 4);
 }
 
 fn make_vga_entry(uc: u8, new_color: u8) u16 {
-    const c: u16 = new_color;
-    return uc | (c << 8);
+    return uc | (@as(u16, new_color) << 8);
 }
 
-fn port_output_b(port: u16, data: u8) void {
-    asm volatile (
-        \\ outb %al, %dx
-        :
-        : [port] "{dx}" (port), [data] "{al}" (data)
-    );
-}
+const FB_COMMAND_PORT = 0x3D4;
+const FB_DATA_PORT = 0x3D5;
+
+const FB_HIGH_BYTE_COMMAND = 14;
+const FB_LOW_BYTE_COMMAND = 15;
 
 fn set_pos(index: usize) void {
-    Port.output_b(
-        @intFromEnum(Port.Port.FB_COMMAND),
-        @intFromEnum(Port.Command.FB_HIGH_BYTE)
-    );
-    Port.output_b(
-        @intFromEnum(Port.Port.FB_DATA), 
-        @intCast((index >> 8) & 0xFF)
-    );
-    Port.output_b(
-        @intFromEnum(Port.Port.FB_COMMAND), 
-        @intFromEnum(Port.Command.FB_LOW_BYTE)
-    );
-    Port.output_b(
-        @intFromEnum(Port.Port.FB_DATA), 
-        @intCast(index & 0xFF)
-    );
+    Ports.output_b(FB_COMMAND_PORT, FB_HIGH_BYTE_COMMAND);
+    Ports.output_b(FB_DATA_PORT, @intCast((index >> 8) & 0xFF));
+    Ports.output_b(FB_COMMAND_PORT, FB_LOW_BYTE_COMMAND);
+    Ports.output_b(FB_DATA_PORT, @intCast(index & 0xFF));
 }
 
 pub fn init() void {
@@ -71,7 +56,7 @@ pub fn init() void {
 }
 
 pub fn clear() void {
-    @memset(buffer[0..VGA_SIZE], make_vga_entry(' ', color));
+    @memset(base[0..VGA_SIZE], make_vga_entry(' ', color));
     y = 0;
     x = 0;
     set_pos(0);
@@ -86,7 +71,7 @@ pub fn putch(c: u8) void {
         return;
     }
 
-    buffer[y * VGA_WIDTH + x] = make_vga_entry(c, color);
+    base[y * VGA_WIDTH + x] = make_vga_entry(c, color);
     x += 1;
     if (x == VGA_WIDTH) {
         x = 0;
